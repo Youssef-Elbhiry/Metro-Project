a5a6edf5875af0662d8804cc079fcdbaec62c15c
Youssef <ywsfalbhyry69@gmail.com>
2025-07-24 22:28:11 +0300

    4commit

---
+++ Third Page/script.js
@@ -0,0 +1,12 @@
+let quanty = 1
+
+function increase(){
+    quanty++
+     document.getElementById("q").innerText = quanty
+}
+
+
+function decrease(){
+    quanty--
+   document.getElementById("q").innerText = quanty
+}
